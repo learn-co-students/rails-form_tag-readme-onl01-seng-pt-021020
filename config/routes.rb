86b1e8a5e0cee7151d1d6cee51bcb8ed39c99b63
github username <github email address>
2020-06-11 16:51:03 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index]
+  # remember that methods are defined here in the routes.rb, and ACTIONS are defined in the controller.
+  resources :posts, only: [:index, :new, :create]
+
 end
